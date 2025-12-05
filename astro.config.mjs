5f937d6933ed578411aad9e592c820abaa7b138c
Gustavo Costa <gustavo@arcos.org.br>
2025-12-05 18:53:19 -0300

    implement custom url on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    site: 'https://noah-art3mis.github.io',
-    base: '/simulacro-tech',
+    site: 'https://simulacro.tech',
 });
